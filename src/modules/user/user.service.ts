d49b8288a63e568e485fb5b761b6333f9fe6882d
Chinh Nguyen <chinhnguyennn24@gmail.com>
2025-02-03 17:15:53 +0700

    Test CICD 1

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,22 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
+  create() {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all user`;
+    return `This action returns all user CICD`;
   }
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  update(id: number) {
     return `This action updates a #${id} user`;
   }
 
